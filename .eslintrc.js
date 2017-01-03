3b8495386edb10a156f63adeb6fb2f75d67e7997
Justin Falcone <stanulam59@gmail.com>
2017-01-03 01:18:18 -0500

    initial commit?

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true,
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-multi-spaces": [
+            "off",
+        ],
+        "no-console": [
+            "off",
+        ],
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double",
+            { avoidEscape: true, allowTemplateLiterals: true }
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
